12d265de883e41fd78409481ab495c6f29ed81e7
liyan <liyangeneral@gmail.com>
2025-10-26 13:52:48 -0700

    delete a book

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -104,4 +104,11 @@ def update_book(book_id):
     return Response(status=204, mimetype="application/json")
 
 
+@books_bp.delete("/<book_id>")
+def delete_book(book_id):
+    book = validate_book(book_id)
 
+    db.session.delete(book)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
